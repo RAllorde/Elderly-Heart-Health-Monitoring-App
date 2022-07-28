ff37db21c4e2fad804df42bf5c1a3114de290526
RAllorde <ranceallorde@gmail.com>
2022-07-28 22:31:16 +0800

    edited .exe conversion command

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     os.system("pip install requests")
     os.system("pip install pyinstaller")
     os.system("pip install sklearn")
-    os.system("pyinstaller -F --onefile --add-data user_List.py;MAIN.py --add-data prediction_List.py;MAIN.py --add-data TEST.py;MAIN.py MAIN.py")
+    os.system('pyinstaller --noconfirm --onefile --windowed --add-data "E:/Desktop/SCHOOL/CPE126/SERVICE LEARNING/CODE/Initialize.py;." --add-data "E:/Desktop/SCHOOL/CPE126/SERVICE LEARNING/CODE/prediction_List.py;." --add-data "E:/Desktop/SCHOOL/CPE126/SERVICE LEARNING/CODE/setup.py;." --add-data "E:/Desktop/SCHOOL/CPE126/SERVICE LEARNING/CODE/user_List.py;."  "E:/Desktop/SCHOOL/CPE126/SERVICE LEARNING/CODE/MAIN.py"')
