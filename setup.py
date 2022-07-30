9d6e5b22a3261f1282ac97992dafb66642462b19
RAllorde <ranceallorde@gmail.com>
2022-07-30 17:34:35 +0800

    created modern gui and arranged .exe conversion command accrodingly

--- setup.py
+++ setup.py
@@ -6,21 +6,19 @@ if __name__ == '__main__':
     userPATH = '"' + path + '\\user_List.py;."'
     predictionPATH = '"' + path + '\\prediction_List.py;."'
     initializePATH = '"' + path + '\\Initialize.py;."'
-    imagePATH1 = '"' + path + '\\assets\\button_1.png;."'
-    imagePATH2 = '"' + path + '\\assets\\button_2.png;."'
-    imagePATH3 = '"' + path + '\\assets\\button_3.png;."'
+    imagePATH = '"' + path + '\\images;images\\"'
+    iconPATH = '"' + path + '\\heart.ico"'
 
     mainPATH = mainPATH.replace("\\", "/")
     userPATH = userPATH.replace("\\", "/")
     predictionPATH = predictionPATH.replace("\\", "/")
     initializePATH = initializePATH.replace("\\", "/")
-    imagePATH1 = imagePATH1.replace("\\", "/")
-    imagePATH2 = imagePATH2.replace("\\", "/")
-    imagePATH3 = imagePATH3.replace("\\", "/")
+    imagePATH = imagePATH.replace("\\", "/")
+    iconPATH = iconPATH.replace("\\", "/")
 
     os.system("pip install pysqlite3")
     os.system("pip install requests")
     os.system("pip install pyinstaller")
     os.system("pip install sklearn")
     os.system("pip install Pillow")
-    os.system('pyinstaller --noconfirm --onedir --windowed --add-data ' + imagePATH1 + ' --add-data ' + imagePATH2 + ' --add-data ' + imagePATH3 + ' --add-data ' + initializePATH + ' --add-data ' + predictionPATH + ' --add-data ' + userPATH + ' ' +  mainPATH)
+    os.system('pyinstaller --noconfirm --onedir --windowed --icon ' + iconPATH + ' --add-data ' + imagePATH + ' --add-data ' + initializePATH + ' --add-data ' + predictionPATH + ' --add-data ' + userPATH + ' ' +  mainPATH)
