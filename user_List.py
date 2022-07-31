9c56d5cc3d8d41f0cd4e1fc81a769086b5a85cff
RAllorde <ranceallorde@gmail.com>
2022-08-01 01:44:37 +0800

    removed unused functions

--- user_List.py
+++ user_List.py
@@ -33,36 +33,3 @@ def user_Login(username, password, db):
             log_Message = "Incorrect password"
 
     return log_Message
-
-def user_CheckDatabase(db):
-    db.execute("SELECT * FROM USER")
-    user_List = db.fetchall()
-    return user_List
-
-def user_EditUsername(username_old, username_new, db):
-    db.execute("SELECT Username FROM USER WHERE Username=:Username", 
-        {'Username': username_old})
-    
-    if (type(db.fetchone()) == NoneType):
-        log_Message = "User does not exist"
-    
-    else:
-        db.execute("UPDATE USER set Username=:Username_new WHERE Username=:Username_old", 
-            {'Username_new': username_new, 'Username_old': username_old})
-
-        log_Message = "Username changed"
-    return log_Message
-
-def user_EditPassword(username, password_new, db):
-    db.execute("SELECT Username FROM USER WHERE Username=:Username", 
-        {'Username': username})
-    
-    if (type(db.fetchone()) == NoneType):
-        log_Message = "User does not exist"
-
-    else:
-        db.execute("UPDATE USER set Password=:Password_new WHERE Username=:Username", 
-            {'Password_new': password_new, 'Username': username})
-            
-        log_Message = "Password changed"
-    return log_Message
